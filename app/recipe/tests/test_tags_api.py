c22a2850a33dce626fa574522f74ea258b9ec03f
Eugene Shevchuk <evgeniy.shevchuk@gmail.com>
2021-02-16 10:25:06 +0300

    Added create tag feature

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -85,3 +85,12 @@ class PrivateTagsApiTests(TestCase):
         payload = {'name': ''}
         res = self.client.post(TAGS_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_ingredient_str(self):
+        """Test the ingredient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Cucumber'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
