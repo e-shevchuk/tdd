04d39b6e82a466d66eca441a1c45791d0c662967
Eugene Shevchuk <evgeniy.shevchuk@gmail.com>
2021-02-17 11:32:59 +0300

    Added custom recepie detail view

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -39,3 +39,9 @@ class RecipeSerializer(serializers.ModelSerializer):
             'price', 'link'
         )
         read_only_fields = ('id',)
+
+
+class RecepieDetailSerializer(RecipeSerializer):
+    """Serialize a recepie detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
